27a41ca3d69bf083fbb70d41662d841dcb39a505
PRAS Martin <martin.pras@epitech.eu>
2016-08-22 14:12:33 +0200

    correction petit bug + orientation seuelemnt vertical

--- www/js/favoris.js
+++ www/js/favoris.js
@@ -1,34 +1,36 @@
 $(document).ready(function(){
-                                    id = $(".titrefiche").attr('id');
-                                    value = localStorage.getItem(id);
-                                    if (value == "true")
-                                    {
-                                    $(".fav").toggleClass("fav_plein");
-                                    $(".bookmark").css("background-color", "white");
-                                    $(".bookmark").css("border", "1px solid #D3D9E4")
-                                    }
-                                    else
-                                    {
-                                    $(".fav_plein").toggleClass("fav");
-                                    $(".bookmark").css("background-color", "#59e09e");
-                                    $(".bookmark").css("border", "0px")
-                                    }
-                                    $(".bookmark").click(function(){
-                                                         id = $(".titrefiche").attr('id');
-                                                         value = localStorage.getItem(id);
-                                                         if (value == "true")
-                                                         {
-                                                         $(".fav").attr('src', '../../../img/fav_vide.png');
-                                                         $(".bookmark").css("background-color", "#59e09e");	
-                                                         $(".bookmark").css("border", "0px")
-                                                         localStorage.setItem(id, false)
-                                                         }
-                                                         else
-                                                         {
-                                                         $(".fav").attr('src', '../../../img/fav.png');
-                                                         $(".bookmark").css("background-color", "white");
-                                                         $(".bookmark").css("border", "1px solid #D3D9E4")	
-                                                         localStorage.setItem(id, true)
-                                                         }
-                                                         });
+   id = $(".titrefiche").attr('id');
+   value = localStorage.getItem(id);
+   if (value == "true")
+   {
+      $(".fav").attr('src', '../../../img/fav.png');
+      $(".bookmark").css("background-color", "white");
+      $(".bookmark").css("border", "1px solid #D3D9E4")  
+   }
+   else
+   {
+      $(".fav").attr('src', '../../../img/fav_vide.png');
+      $(".bookmark").css("background-color", "#59e09e"); 
+      $(".bookmark").css("border", "0px")
+   }
+
+   $(".bookmark").click(function(){
+      id = $(".titrefiche").attr('id');
+      value = localStorage.getItem(id);
+      if (value == "true")
+      {
+         $(".fav").attr('src', '../../../img/fav_vide.png');
+         $(".bookmark").css("background-color", "#59e09e");	
+         $(".bookmark").css("border", "0px")
+         localStorage.setItem(id, false)
+      }
+      else
+      {
+         $(".fav").attr('src', '../../../img/fav.png');
+         $(".bookmark").css("background-color", "white");
+         $(".bookmark").css("border", "1px solid #D3D9E4")	
+         localStorage.setItem(id, true)
+      }
+   });
+
 });
